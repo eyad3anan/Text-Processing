85cc0a37ace7a32c376872375fddae3f00c4f177
eyad3anan <128145472+eyad3anan@users.noreply.github.com>
2024-04-24 13:29:50 +0200

    Update text_proccessing.py

--- text_proccessing.py
+++ text_proccessing.py
@@ -1,14 +1,12 @@
 import nltk # importing the library nltk which stands for (natural Language toolkit)
 from nltk.corpus import stopwords # importing the dataset stopwords from the module corpus
-# stopwords ==> is a dataset containing a collection of stop words
-import pandas as pd # Importing the python library
+import pandas as pd 
 nltk.download("stopwords") # to download the stopwords dataset which contains a collection of stoping words
 nltk.download("punkt")
 data_frame = pd.read_csv("random_paragraphs.txt" , sep = "\t" , header = None) # Reading the txt file 
-text_column = data_frame.iloc[:, 0] # Taking all the data in the first row
+text_column = data_frame.iloc[:, 0] 
 raw_text = ' '.join(text_column) # Convert the text data to a single string
-# print(raw_text) # printing the raw text
-separated_words = nltk.word_tokenize(raw_text) # word_tokenize(): This is the most common type of tokenization, where text is divided into words based on whitespace or punctuation
+separated_words = nltk.word_tokenize(raw_text) 
 
 english_stop_words = set(stopwords.words('english')) # getting the stop words in the english
 
